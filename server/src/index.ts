0dc34d594f2ca5f15a5a3a10a6a50c675d515c00
Anastasiia Ustin <2anastasiia@mail.ru>
2026-08-16 20:40:58 +0200

    feat: add server router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import http from "http";
+import router from "./router.ts";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(router);
 
 const httpServer = http.createServer(app);
 
