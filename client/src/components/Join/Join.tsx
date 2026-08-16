50a4cef2f6add4eb9f2cefdc5635a72bf506f8c5
Anastasiia Ustin <2anastasiia@mail.ru>
2026-08-16 21:05:17 +0200

    feat: make controlled inputs for name and room properties

--- client/src/components/Join/Join.tsx
+++ client/src/components/Join/Join.tsx
@@ -1,16 +1,30 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Join: FC = () => {
+  const [name, setName] = useState<string>("");
+  const [room, setRoom] = useState<string>("");
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
         <div>
-          <input type="text" placeholder="Name" className="joinInput" />
+          <input
+            value={name}
+            type="text"
+            placeholder="Name"
+            className="joinInput"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div>
-          <input type="text" placeholder="Room" className="joinInput mt-20" />
+          <input
+            value={room}
+            type="text"
+            placeholder="Room"
+            className="joinInput mt-20"
+            onChange={(e) => setRoom(e.target.value)}
+          />
         </div>
         <Link to={""}>
           <button className="button mt-20" type="submit"></button>
