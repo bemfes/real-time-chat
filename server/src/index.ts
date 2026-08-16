fe8bc947f9b0b0fa651da2c1a43d38181f5e387e
Anastasiia Ustin <2anastasiia@mail.ru>
2026-08-16 20:50:53 +0200

    feat: create socket io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 import router from "./router.ts";
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,15 @@ const app = express();
 app.use(router);
 
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("user is connected");
+  socket.on("disconnect", () => {
+    console.log("user is disconnected");
+  });
+});
 
 httpServer.listen(PORT, () =>
   console.log(`server has started on port ${PORT}`),
