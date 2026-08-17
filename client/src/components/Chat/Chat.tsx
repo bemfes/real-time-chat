c2e28f847fd43521c4aae5e5ca82c1a75622db2c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-08-17 19:42:34 +0200

    feat: create socket

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,10 +1,19 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import io from "socket.io-client";
 
 const Chat: FC = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name");
   const room = searchParams.get("room");
+
+  const ENDPOINT = "http://localhost:3000";
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    console.log(socket);
+  }, [ENDPOINT]);
+
   return (
     <div>
       chat with user {name} in room {room}
