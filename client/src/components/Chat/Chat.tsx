85fcb502e23d2ef13581185e91e7bd6fd4c53ca1
Anastasiia Ustin <2anastasiia@mail.ru>
2026-08-19 17:27:09 +0200

    feat: create basic html for Chat

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import io, { Socket } from "socket.io-client";
 
@@ -6,6 +6,8 @@ const Chat: FC = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name");
   const room = searchParams.get("room");
+
+  const [message, setMessage] = useState<string>("");
 
   const ENDPOINT = "http://localhost:3000";
 
@@ -24,9 +26,19 @@ const Chat: FC = () => {
     };
   }, [ENDPOINT, name, room]);
 
+  const sendMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e);
+  };
+
   return (
-    <div>
-      chat with user {name} in room {room}
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+        />
+      </div>
     </div>
   );
 };
